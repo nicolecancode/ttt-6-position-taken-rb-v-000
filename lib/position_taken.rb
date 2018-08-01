bf75e195ad67a62a5d6118a797c322469c88296c
Nicole Pretorius <nicole.pretoriusuk+learn@gmail.com>
2018-08-01 16:35:48 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 
 
-def position_taken?(board, index)
+def position_taken?(board, index_number)
   if board[index_number] == " " || board[index_number] == "" || board[index_number] == nil
     false
   else
